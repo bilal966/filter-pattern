eccd571417fe8fba027eb042a2ba08a03aeb47b6
bilal <bilal.ilyas@mobilelive.ca>
2018-04-19 19:22:50 +0500

    init commit structured project setup

---
+++ src/main/java/com/design/pattern/filterpattern/FilterPatternApplication.java
@@ -0,0 +1,12 @@
+package com.design.pattern.filterpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterPatternApplication.class, args);
+	}
+}
